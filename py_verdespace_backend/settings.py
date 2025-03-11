b6e55cbb02da7c59edea7909cbdd1bba8ec5bf96
Ivan Havrylyshyn <jyjuk1@gmail.com>
2025-03-11 12:38:18 +0200

    add rest framework simplejwt in installed apps

--- py_verdespace_backend/settings.py
+++ py_verdespace_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
+    "rest_framework",
+    "rest_framework_simplejwt",
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
