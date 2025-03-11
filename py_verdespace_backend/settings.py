3fb7b12ef0cfe74db3fbd488266e1239c7de970b
Ivan Havrylyshyn <jyjuk1@gmail.com>
2025-03-11 12:19:23 +0200

    add user app in installed apps & set auth user model

--- py_verdespace_backend/settings.py
+++ py_verdespace_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
